dd73d989a2d3c251639ae19362662d6dfce99d26
Vincent Tourraine <me@vtourraine.net>
2016-04-02 10:55:08 +0200

    Clean code

--- mixit/main.m
+++ mixit/main.m
@@ -3,15 +3,14 @@
 //  mixit
 //
 //  Created by Vincent Tourraine on 30/04/14.
-//  Copyright (c) 2014-2015 Studio AMANgA. All rights reserved.
+//  Copyright (c) 2014-2016 Studio AMANgA. All rights reserved.
 //
 
 @import UIKit;
 
 #import "AMGAppDelegate.h"
 
-int main(int argc, char * argv[])
-{
+int main(int argc, char * argv[]) {
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass(AMGAppDelegate.class));
     }
