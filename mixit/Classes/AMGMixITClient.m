65e0ed15f21759981e10176ad2dbb3ff3eb6b2e7
Vincent Tourraine <me@vtourraine.net>
2017-02-23 17:20:43 +0100

    Clean code

--- mixit/Classes/AMGMixITClient.m
+++ mixit/Classes/AMGMixITClient.m
@@ -3,7 +3,7 @@
 //  mixit
 //
 //  Created by Vincent Tourraine on 01/05/14.
-//  Copyright (c) 2014-2016 Studio AMANgA. All rights reserved.
+//  Copyright (c) 2014-2017 Studio AMANgA. All rights reserved.
 //
 
 #import "AMGMixITClient.h"
@@ -22,8 +22,8 @@ static NSString * const AMGMixITAPIBaseURLString = @"https://www.mix-it.fr/api/"
 }
 
 + (instancetype)MixITClient {
-    NSURL          *baseURL = [NSURL URLWithString:AMGMixITAPIBaseURLString];
-    AMGMixITClient *client  = [[AMGMixITClient alloc] initWithBaseURL:baseURL];
+    NSURL *baseURL = [NSURL URLWithString:AMGMixITAPIBaseURLString];
+    AMGMixITClient *client = [[AMGMixITClient alloc] initWithBaseURL:baseURL];
 
     return client;
 }
